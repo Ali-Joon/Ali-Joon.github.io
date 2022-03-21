2355a2f8f1cb24ebffd5462f31247e5b4e5de750
Ali-Joon <48525075+Ali-Joon@users.noreply.github.com>
2022-03-21 09:39:10 +0330

    Added copy function

--- index.js
+++ index.js
@@ -109,4 +109,10 @@ function t() {
 		clearInterval(timer);
 	}
 }
- 
+function copyToClipboard(element) {
+  var $temp = $("<input>");
+  $("body").append($temp);
+  $temp.val($(element).text()).select();
+  document.execCommand("copy");
+  $temp.remove();
+}
